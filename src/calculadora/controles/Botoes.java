550829b9cf4a560896ffe71badb12c9c168b4a0c
tiago <tonon2002@email.aqui>
2022-06-09 17:47:09 -0300

    versao final

--- src/calculadora/controles/Botoes.java
+++ src/calculadora/controles/Botoes.java
@@ -15,16 +15,7 @@ public enum Botoes {
         @Override
         public void selecaoBotao() {
 
-            if (!Display.getTextoLabel().equals("0")) {
-
-                if (Display.getTextoLabel().contains("-")){
-                    Display.setTextoLabel(Display.getTextoBuffer().substring(1));
-                    return;
-                }
-                Display.setTextoLabel("-"+Display.getTextoLabel());
-
-
-            }
+          SetNumeros.sinal();
 
 
         }
@@ -49,47 +40,21 @@ public enum Botoes {
     }, BOTAOMENOS {
         @Override
         public void selecaoBotao() {
-            float result;
-            boolean inteiro;
-            if (!Display.getTextoLabel().equals("0")&&Display.getTextoBuffer().equals("0")) {
-                Display.setTextoBuffer(Display.getTextoLabel());
-                Display.setTextoLabel("0");
-                Display.setBufferLogica("-");
-                return;
-            }
-            result=Float.parseFloat(Display.getTextoBuffer())-Float.parseFloat(Display.getTextoLabel());
-            inteiro=Float.toString(result).endsWith(".0");
-            Display.setTextoLabel(inteiro?Float.toString(result).replace(".0",""):Float.toString(result));
-            Display.setTextoBuffer("0");
-            Display.setBufferLogica("");
+          SetNumeros.subtracao();
 
         }
     }, BOTAOVEZES {
         @Override
         public void selecaoBotao() {
-            float result;
-            boolean inteiro;
-            if (!Display.getTextoLabel().equals("0")&&Display.getTextoBuffer().equals("0")) {
-                Display.setTextoBuffer(Display.getTextoLabel());
-                Display.setTextoLabel("0");
-                Display.setBufferLogica("x");
-
-                return;
-            }
-
-            result=Float.parseFloat(Display.getTextoBuffer())*Float.parseFloat(Display.getTextoLabel());
-            inteiro=Float.toString(result).endsWith(".0");
-            Display.setTextoLabel(inteiro?Float.toString(result).replace(".0",""):Float.toString(result));
-            Display.setTextoBuffer("0");
-            Display.setBufferLogica("");
-
+         SetNumeros.vezes();
         }
     },
     BOTAO1 {
         @Override
         public void selecaoBotao() {
             SetNumeros.setNumero("1");
-    }}, BOTAO2 {
+    }},
+    BOTAO2 {
         @Override
         public void selecaoBotao() {
                 SetNumeros.setNumero("2");
--- src/calculadora/controles/SetNumeros.java
+++ src/calculadora/controles/SetNumeros.java
@@ -61,5 +61,52 @@ public  static void divisao() {
     Display.setTextoBuffer("0");
     Display.setBufferLogica("");
 }
+
+    public static void subtracao() {
+        float result;
+        boolean inteiro;
+        if (!Display.getTextoLabel().equals("0")&&Display.getTextoBuffer().equals("0")) {
+            Display.setTextoBuffer(Display.getTextoLabel());
+            Display.setTextoLabel("0");
+            Display.setBufferLogica("-");
+            return;
+        }
+        result=Float.parseFloat(Display.getTextoBuffer())-Float.parseFloat(Display.getTextoLabel());
+        inteiro=Float.toString(result).endsWith(".0");
+        Display.setTextoLabel(inteiro?Float.toString(result).replace(".0",""):Float.toString(result));
+        Display.setTextoBuffer("0");
+        Display.setBufferLogica("");
     }
+
+
+    public static void vezes() {
+        float result;
+        boolean inteiro;
+        if (!Display.getTextoLabel().equals("0")&&Display.getTextoBuffer().equals("0")) {
+            Display.setTextoBuffer(Display.getTextoLabel());
+            Display.setTextoLabel("0");
+            Display.setBufferLogica("x");
+
+            return;
+        }
+
+        result=Float.parseFloat(Display.getTextoBuffer())*Float.parseFloat(Display.getTextoLabel());
+        inteiro=Float.toString(result).endsWith(".0");
+        Display.setTextoLabel(inteiro?Float.toString(result).replace(".0",""):Float.toString(result));
+        Display.setTextoBuffer("0");
+        Display.setBufferLogica("");
+    }
+
+    public static void sinal() {
+        if (!Display.getTextoLabel().equals("0")) {
+
+            if (Display.getTextoLabel().contains("-")){
+                Display.setTextoLabel(Display.getTextoBuffer().substring(1));
+                return;
+            }
+            Display.setTextoLabel("-"+Display.getTextoLabel());
+
+        }
+    }
+}
 
